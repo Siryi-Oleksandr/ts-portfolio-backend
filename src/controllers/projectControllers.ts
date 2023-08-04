f3765b780a5d14c0ed277d6e6d7ed919977ade5c
Oleksandr Siryi <siryyoleksandr@gmail.com>
2023-08-04 22:32:33 +0300

    fix adding 3 files

--- src/controllers/projectControllers.ts
+++ src/controllers/projectControllers.ts
@@ -52,9 +52,11 @@ const addProject = controllerWrapper(async (req: any, res: Response) => {
   // Upload each poster to Cloudinary
   const uploadedPosters = await Promise.all(
     req.files.map(async (file: Express.Multer.File) => {
+      await fs.access(file.path);
       const fileData = await cloudinaryProjectAPI.upload(file.path);
       const posterURL = fileData.url;
       const posterID = fileData.public_id;
+
       await fs.unlink(file.path);
       return { posterURL, posterID };
     })
